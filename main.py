439aa926384bdedefccdf837c1fe7fb4ac8d5f41
Joey Leung <joeyleung98@gmail.com>
2024-02-03 18:37:00 +0000

    linked javascript to hit python endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Query, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
@@ -11,3 +11,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get('/', response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post('/mood')
+async def set_mood(mood: str = Query(..., description="The mood parameter in the query")):
+    print(mood)
+    # Do something with the mood parameter
+    return {"message": f'Mood set successfully to {mood}'}
